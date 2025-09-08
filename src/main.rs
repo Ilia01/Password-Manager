73e91ecbfff23e71c2c5f67cf422bd33b002a038
Ilia01 <ilia.goginashvili.1@btu.edu.ge>
2025-09-08 20:45:32 +0400

    upgraded to proper-looking cli password manager

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use clap::{Parser, Subcommand};
+use copypasta::{ClipboardContext, ClipboardProvider};
+use rand::{Rng, rng};
+use std::io;
+use std::io::Write;
 use std::{
-    fs::{self},
-    io::Write,
+    fs::{self, OpenOptions},
     path::Path,
 };
+
+use crossterm::event::{Event, KeyCode, read};
+use crossterm::execute;
+use crossterm::style::{Attribute, Color, Print, ResetColor, SetAttribute, SetForegroundColor};
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use passwords::analyzer;
+use passwords::scorer;
+
+#[derive(PartialEq)]
+enum WeakPasswordChoice {
+    ABORT,
+    CONTINUE,
+}
 
 #[derive(Parser)]
 struct Cli {
@@ -15,38 +32,152 @@ struct Cli {
 enum Commands {
     List,
     Add {
+        #[arg(short, long)]
         service: String,
+        #[arg(short, long)]
         username: String,
-        password: String,
+        #[arg(short, long, default_missing_value("true"))]
+        clipboard: bool, // copy from clipboard
+        #[arg(short, long, default_missing_value("true"))]
+        generate: bool, // generate new password
+        #[arg(short, long, default_missing_value("true"))]
+        write: bool, // type new password
     },
 }
 
 fn main() -> std::io::Result<()> {
     let args = Cli::parse();
     match args.cmd {
-        Commands::List => display_passwords(),
+        Commands::List => display_passwords()?,
         Commands::Add {
             service,
             username,
-            password,
-        } => add_new_password(service, username, password)?,
+            clipboard,
+            generate,
+            write,
+        } => {
+            if generate {
+                let password = generate_password();
+                println!("{}", password);
+                let _ = add_new_password(&service, &username, &password);
+            }
+
+            if clipboard {
+                let password = get_clipboard_password();
+                println!("{}", password);
+                if should_save_password(&password) {
+                    let _ = add_new_password(&service, &username, &password);
+                }
+            }
+
+            if write {
+                let password = get_user_input().unwrap();
+                if should_save_password(&password) {
+                    let _ = add_new_password(&service, &username, &password);
+                }
+            }
+        }
     }
     Ok(())
 }
 
-fn display_passwords() {
-    let path = Path::new("./passwords.txt");
-    let contents = fs::read_to_string(path).expect("Could not read the file");
-    println!("{}", contents);
+fn get_user_input() -> io::Result<String> {
+    println!("Write it down the press Enter!");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => return Ok(input.trim().to_string()),
+        Err(error) => Err(error),
+    }
 }
 
-fn add_new_password(service: String, username: String, password: String) -> std::io::Result<()> {
-    let path = Path::new("./passwords.txt")
-        .canonicalize()
-        .expect("Could not find the file");
+fn get_clipboard_password() -> String {
+    let mut ctx = ClipboardContext::new().unwrap();
+    return ctx.get_contents().unwrap();
+}
+
+fn generate_password() -> String {
+    let charset: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                           abcdefghijklmnopqrstuvwxyz\
+                           0123456789\
+                           !@#$%^&*()_-+=[{]}\\;:'\",<.>/?";
+    let mut rng = rng();
+    let password = (0..16)
+        .map(|_| {
+            let idx = rng.random_range(0..charset.len());
+            charset[idx] as char
+        })
+        .collect();
+    password
+}
+
+fn should_save_password(password: &str) -> bool {
+    if is_password_weak(password) {
+        print_alert(password);
+        if let Ok(choice) = read_next_char() {
+            return choice == WeakPasswordChoice::CONTINUE;
+        } else {
+            return false;
+        }
+    } else {
+        return true;
+    }
+}
+
+fn print_alert(password: &str) {
+    let alert = format!(
+        "{} is a weak password. Press Enter to continue anyway. Press Q to abort and try again!",
+        password
+    );
+    execute!(
+        io::stdout(),
+        SetForegroundColor(Color::Red),
+        SetAttribute(Attribute::Bold),
+        Print(alert),
+        ResetColor,
+        SetAttribute(Attribute::Reset)
+    )
+    .unwrap();
+}
+
+fn is_password_weak(password: &str) -> bool {
+    let score = scorer::score(&analyzer::analyze(password));
+    return score < 80.0;
+}
+
+fn read_next_char() -> io::Result<WeakPasswordChoice> {
+    enable_raw_mode()?;
+    let result = loop {
+        match read()? {
+            Event::Key(event) => match event.code {
+                KeyCode::Char('q') | KeyCode::Char('Q') => {
+                    break Ok(WeakPasswordChoice::ABORT);
+                }
+                KeyCode::Enter => {
+                    break Ok(WeakPasswordChoice::CONTINUE);
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    };
+
+    disable_raw_mode()?;
+    result
+}
+
+fn display_passwords() -> std::io::Result<()> {
+    let path = Path::new("./passwords.txt");
+    let contents = fs::read_to_string(path).expect("Could not read the passwords file");
+
+    println!("{}", contents);
+    Ok(())
+}
+
+fn add_new_password(service: &str, username: &str, password: &str) -> std::io::Result<()> {
+    let path = Path::new("./passwords.txt");
     let password_infos = format!("{}|{}|{}\n", service, username, password);
 
-    let mut file = fs::OpenOptions::new().append(true).open(path)?;
+    let mut file = OpenOptions::new().append(true).open(path)?;
 
     file.write_all(password_infos.as_bytes())?;
 
